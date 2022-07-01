ff6632078d42e8ca6677c691cfa722fe43d35b15
Terence Dixon <terencep.dixon@gmail.com>
2022-07-01 10:18:59 -0700

    Deleted old javascript file and added a new one

---
+++ Week5CodingAssignment.js
@@ -0,0 +1,14 @@
+class Animals
+{
+    constructor(name, position)
+    {
+        this.name = name;
+        this.position = position;
+    }
+
+    describe()
+    {
+        return `${this.name} lives in ths zoo`; 
+
+    }
+}
