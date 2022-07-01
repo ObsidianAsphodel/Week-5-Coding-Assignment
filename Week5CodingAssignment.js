a2165d5a8691dbd365a2a47afe1bae36e2f1b695
Terence Dixon <terencep.dixon@gmail.com>
2022-07-01 12:21:20 -0700

    fleshed out shapeBox class

--- Week5CodingAssignment.js
+++ Week5CodingAssignment.js
@@ -1,14 +1,69 @@
-class Animals
+class Shape
 {
-    constructor(name, position)
+    constructor(name, color, sides)
     {
         this.name = name;
-        this.position = position;
+        this.color = color;
+        this.sides = sides;
     }
 
     describe()
     {
-        return `${this.name} lives in ths zoo`; 
+        return `${this.name} is ${this.color} and has ${this.sides} sides`; 
+    }
+}
+class shapeBox
+{
+    constructor()
+    {
+        this.shapes = [];
+    }
 
+    addShapes(shapes)
+    {
+        if(shapes instanceof Shapes)
+        {
+            this.shapes.push[Shape];
+        }
+        else
+        {
+            throw new Error("That's not a shape. Shapes must be instanced.");
+        }
+    }
+
+    describe()
+    {
+        return `${this.name} has ${this.shapes.length} inside of it`;
+    }
+
+    viewShapes()
+    {
+
+    }
+    
+    deleteShapes()
+    {
+
+    }
+
+    start()
+    {
+    let selection = this.showMenuOptions();
+    while (selection != 0 )
+    {
+        switch(selection)
+        {
+            case '1':
+            break;
+            case '2':
+            break;
+            case '3':
+            break;
+            case '4':
+            break;
+            default:
+            break;
+        }
+    }
     }
 }
